1118e8211ae694f7e07f58724e130edb40bd81fd
Meet Patel <meetcpatel906@gmail.com>
2020-05-26 22:13:00 +0530

    Update application.py

--- application.py
+++ application.py
@@ -11,33 +11,8 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    def api_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
-    if 'id' in request.args:
-        id = request.args['id']
-    else:
-        return "Error: No id field provided. Please specify an id."
-
-    #start_url = id#In this example, the link is https://www.instagram.com/p/BdLhfC-HWIi/?taken-by=arianagrande
-    response = requests.get(id)
-    html = response.text
-
-    soup = BeautifulSoup(html, 'lxml')
-    photo_url = soup.find("meta", property="og:image")['content']
-    #print(photo_url)
-
-    dict = {
-            'URL': photo_url,   
-            }
-   # return dict
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
-    return dict
-
-   # return '''<h1>This is Index Page</h1>
-#<p>This is Index Page</p>'''
+    return '''<h1>This is Index Page</h1>
+    <p>This is Index Page</p>'''
 
 
 @app.route('/insta', methods=['GET'])
